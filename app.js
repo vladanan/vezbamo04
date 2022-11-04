b9530dfd35c711b2255da9e4a5755354fc8ee67e
vladanan <vladan.andjelkovic@gmail.com>
2022-11-04 18:12:43 +0100

    tn17

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(fileUpload());
 
 // routes
 app.get('/', (req, res) => {
-  res.render('index.ejs', { title: 'Home' });
+  res.render('./index.ejs', { title: 'Home' });
 });
 
 // pitanja routes
