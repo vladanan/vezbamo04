68445090ec05c2de12ee962e959e8a03be828934
vladanan <vladan.andjelkovic@gmail.com>
2022-11-05 19:26:34 +0100

    ps1

--- models/baza.js
+++ models/baza.js
@@ -1,12 +1,26 @@
 //var mysql = require('mysql');
 const mysql = require('mysql2');
 
-var pool = mysql.createPool({
+//freesqldatabase.com
+/* var pool = mysql.createPool({
     host: process.env.FREE_MYSQL_HOST,
     port: process.env.FREE_MYSQL_PORT,
     user: process.env.FREE_MYSQL_USER,
     password: process.env.FREE_MYSQL_PASSWORD,
     database: process.env.FREE_MYSQL_DATABASE,
+    connectionLimit: 1000,
+    debug: false,
+    waitForConnections: true,
+    queueLimit: 0
+}); */
+
+//planet scale
+var pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DATABASE,
     connectionLimit: 1000,
     debug: false,
     waitForConnections: true,
